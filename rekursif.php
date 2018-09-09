0b2b92a682314440350d54228481ade0292122e6
Gagas Putra Persada <42725322+gagasputra@users.noreply.github.com>
2018-09-09 12:23:55 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,39 @@
 <?php
 
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
+function rekursif($kolom, $baris) {
+    if ($kolom > 0) {
+        if ($baris > 0) {
+            echo "* ";
+            $baris--;
+            rekursif ($kolom, $baris);
+        }
+        else {
+            echo "<br>";
+            $kolom--;
+            rekursif ($kolom, $kolom);
+        }
+    }
 }
-rekursif(5);
+rekursif(5, 5);
+
+echo "<br><br>";
+
+//====================================================================================
+
+function rekursif2($kolom, $baris, $nilai) {
+    if ($kolom <= $nilai) {
+        if ($baris <= $kolom) {
+            echo "$baris ";
+            $baris++;
+            rekursif2 ($kolom, $baris, $nilai);
+        }
+        else {
+            echo "<br>";
+            $kolom++;
+            rekursif2 ($kolom, 1, $nilai);
+        }
+    }
+}
+rekursif2(1, 1, 5);
+
 ?>
